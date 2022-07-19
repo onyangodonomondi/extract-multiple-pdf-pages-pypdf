7aaf0d487583cbd2b732950d1d7695f4a340cbea
Don Omondi <69359811+onyangodonomondi@users.noreply.github.com>
2022-07-19 10:11:16 +0300

    Delete read.py
    Not needed

--- read.py
+++
@@ -1,7 +0,0 @@
-import PyPDF2
-pdffileObj = open("psc.pdf", "rb")
-pdfreader = PyPDF2.PdfFileReader(pdffileObj)
-print(pdfreader.numPages)
-pageobj=pdfreader.getPage(0)
-data=pageobj.extractText()
-print(data)
